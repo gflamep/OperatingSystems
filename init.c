cf8bbfca6103984af5eebc64ccc6e06645507948
jeesup0103 <jeesup0103@gmail.com>
2024-03-17 20:45:00 +0900

    pa0 boot message

--- init.c
+++ init.c
@@ -21,6 +21,9 @@ main(void)
 
   for(;;){
     printf(1, "init: starting sh\n");
+    printf(1, "Student ID: 2020310628\n");
+    printf(1, "Name: Jeesup Park\n");
+    printf(1, "=======GOOD LUCK=======\n");
     pid = fork();
     if(pid < 0){
       printf(1, "init: fork failed\n");
